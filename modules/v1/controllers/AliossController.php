e556aaf20ecbbb1e838124504ee5599319c6ec2d
QC-Promer <yangqichen@promer.co>
2019-09-28 00:13:55 +0800

    debug

--- modules/v1/controllers/AliossController.php
+++ modules/v1/controllers/AliossController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 namespace mhubkol\modules\v1\controllers;
 use common\components\Aliyunoss;
@@ -30,36 +29,4 @@ class AliossController extends Controller
 
 
 }
-=======
-<?php
-namespace mhubkol\modules\v1\controllers;
-use common\components\Aliyunoss;
-use yii\web\Controller;
-/**
- * Site controller
- */
-class AliossController extends Controller
-{
-    public  $enableCsrfValidation=false;
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
 
-    public function  actionIndex(){
-
-           $al = new Aliyunoss();
-        $al->test();
-    }
-
-
-}
->>>>>>> 023f55c9c2306d03c843bf4c73671fb243541e14
