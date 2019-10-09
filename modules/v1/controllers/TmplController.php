eb719353a74259c43ab978c337d19e11913201ca
QC-Promer <yangqichen@promer.co>
2019-10-09 17:08:09 +0800

    modify

--- modules/v1/controllers/TmplController.php
+++ modules/v1/controllers/TmplController.php
@@ -36,7 +36,7 @@ class TmplController extends Controller
 
 
         $tmpl =   new TmplService($formId,41);
-        $send_tmpl=  $tmpl->activitySend('zhangsan','o4Eh85X3JlRuYBktXnX1tRerhRwM',$formId,'2019/10/10',$user_info['phone_number'],'西门庆大战洪教头');
+        $send_tmpl=  $tmpl->activitySend('zhangsan','o4Eh85X3JlRuYBktXnX1tRerhRwM',$formId,'2019/10/10',18751587568,'西门庆大战洪教头');
 
         print_r($send_tmpl);
 
