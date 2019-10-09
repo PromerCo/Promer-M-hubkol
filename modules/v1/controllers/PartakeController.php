2b7963b6edbe8c506c5dbee360cf716954bdf070
QC-Promer <yangqichen@promer.co>
2019-10-09 17:33:06 +0800

    modify

--- modules/v1/controllers/PartakeController.php
+++ modules/v1/controllers/PartakeController.php
@@ -121,6 +121,8 @@ WHERE hubkol_push.id = $push_id
                       if (empty($tmpl_msg['open_id']) || empty($tmpl_msg['form_id']) || empty($tmpl_msg['title'])){
                           return  HttpCode::renderJSON([],'参数不存在','416');
                       }
+
+                      return  HttpCode::renderJSON([],$user_info['nick_name'],'416');
                       $send_tmpl=  $tmpl->activitySend($user_info['nick_name'],$tmpl_msg['open_id'],$tmpl_msg['form_id'],'2019/10/10','18511587569',$tmpl_msg['title']);
                       if ($send_tmpl['errcode'] == 0){
                           $transaction->commit();  //提交事务
