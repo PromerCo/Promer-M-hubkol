84671030747dc4e2248b3be3dd74b4be806b6e16
QC-Promer <yangqichen@promer.co>
2019-10-09 17:12:01 +0800

    modify

--- modules/v1/controllers/PartakeController.php
+++ modules/v1/controllers/PartakeController.php
@@ -117,8 +117,12 @@ WHERE  hubkol_push.id = $push_id AND   hubkol_kol.uid=$this->uid")->asArray()->o
 JOIN hubkol_user ON hubkol_push.uid = hubkol_user.id
 WHERE hubkol_push.id = $push_id
 ")->asArray()->one();
+                      if (empty($tmpl_msg['open_id']) || empty($tmpl_msg['form_id'])){
+                          return  HttpCode::renderJSON([],'参数不存在','416');
+                      }
 
-                      $send_tmpl=  $tmpl->activitySend($user_info['nick_name'],$tmpl_msg['open_id'],$tmpl_msg['form_id'],'2019/10/10',$user_info['phone_number'],'西门庆大战洪教头');
+
+                      $send_tmpl=  $tmpl->activitySend($enroll_add['nick_name'],$tmpl_msg['open_id'],$tmpl_msg['form_id'],'2019/10/10',$enroll_add['phone_number'],'西门庆大战洪教头');
                       if ($send_tmpl['errcode'] == 0){
                           $transaction->commit();  //提交事务
                           return  HttpCode::renderJSON($user_info['avatar_url'],'报名成功','201');
