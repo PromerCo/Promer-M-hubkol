b6012091e2a990d4df0679706b13ceece17e536e
QC-Promer <yangqichen@promer.co>
2019-10-10 17:11:24 +0800

    modify

--- modules/v1/controllers/KolController.php
+++ modules/v1/controllers/KolController.php
@@ -101,7 +101,8 @@ LEFT JOIN hubkol_platform ON hubkol_platform.id = hubkol_kol.platform where  hub
                 $data =    HubkolPush::findBySql("SELECT  hubkol_push.id,hubkol_push.title,hubkol_platform.logo,hubkol_push.create_date FROM  hubkol_push
 LEFT JOIN  hubkol_hub ON hubkol_push.hub_id = hubkol_hub.id
 LEFT JOIN  hubkol_platform ON hubkol_platform.id = hubkol_push.platform
-WHERE hubkol_hub.uid =$uid  ORDER BY hubkol_push.create_date desc")->asArray()->all();
+WHERE hubkol_hub.uid =$uid AND hubkol_push.expire_time > NOW()
+ORDER BY hubkol_push.create_date desc")->asArray()->all();
                 return  HttpCode::renderJSON($data,'ok','201');
             break;
             case 2:
@@ -110,6 +111,7 @@ LEFT JOIN  hubkol_kol ON hubkol_pull.kol_id = hubkol_kol.id
 LEFT JOIN hubkol_push ON hubkol_pull.push_id = hubkol_push.id
 LEFT JOIN  hubkol_platform ON hubkol_platform.id = hubkol_push.platform
 WHERE hubkol_kol.uid =$uid  AND hubkol_pull.is_enroll = 1
+AND hubkol_push.expire_time > NOW()
 ORDER BY hubkol_push.create_date desc")->asArray()->all();
                 return  HttpCode::renderJSON($data,'ok','201');
             break;
