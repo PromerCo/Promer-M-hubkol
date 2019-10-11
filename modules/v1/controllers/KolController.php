0b8d351349c49646d4b7e429f078637920ca96af
QC-Promer <yangqichen@promer.co>
2019-10-11 19:03:33 +0800

    modify

--- modules/v1/controllers/KolController.php
+++ modules/v1/controllers/KolController.php
@@ -80,7 +80,7 @@ hubkol_platform.logo,hubkol_platform.id as platform_id  FROM  hubkol_kol
 LEFT JOIN hubkol_user ON hubkol_kol.uid = hubkol_user.id
 LEFT JOIN  hubkol_follow ON  hubkol_follow.id = hubkol_kol.follow_level
 LEFT JOIN hubkol_platform ON hubkol_platform.id = hubkol_kol.platform where  hubkol_kol.platform = $platform_id
- and   WHERE hubkol_user.id  != $this->uid
+ AND   hubkol_user.id  != $this->uid
  LIMIT $start_page,5")->asArray()->all();
             }
 
