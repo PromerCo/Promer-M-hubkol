ba6df9bf6b633703efa09d292fcf6f2d550d172e
QC-Promer <yangqichen@promer.co>
2019-10-11 21:09:08 +0800

    modify

--- modules/v1/controllers/KolController.php
+++ modules/v1/controllers/KolController.php
@@ -319,10 +319,10 @@ WHERE hubkol_kol.id = $pro_id")->asArray()->one();
        $type = \Yii::$app->request->post('type')??0;
        if ($type == 0){
            //关注
-           $data =   HubkolUser::findBySql("SELECT avatar_url,nick_name,IF(capacity = 1,'HUB','KOL') as capacity FROM  hubkol_user WHERE  id =(SELECT kol_id FROM hubkol_carefor WHERE hub_id = $this->uid)")->asArray()->all();
+           $data =   HubkolUser::findBySql("SELECT avatar_url,nick_name,IF(capacity = 1,'HUB','KOL') as capacity FROM  hubkol_user WHERE  id  in (SELECT kol_id FROM hubkol_carefor WHERE hub_id = $this->uid)")->asArray()->all();
        }else{
            //粉丝
-           $data =   HubkolUser::findBySql("SELECT avatar_url,nick_name,IF(capacity = 1,'HUB','KOL') as capacity FROM  hubkol_user WHERE  id =(SELECT kol_id FROM hubkol_carefor WHERE kol_id = $this->uid)")->asArray()->all();
+           $data =   HubkolUser::findBySql("SELECT avatar_url,nick_name,IF(capacity = 1,'HUB','KOL') as capacity FROM  hubkol_user WHERE  id in (SELECT kol_id FROM hubkol_carefor WHERE kol_id = $this->uid)")->asArray()->all();
        }
        return  HttpCode::jsonObj($data,'ok','201');
    }
